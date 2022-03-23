bc96ab843918a91e1446e126151bad5bf13aeff0
Peter Fischer <spotted@gmail.com>
2022-03-23 06:50:48 -0400

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Peter Fischer on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
